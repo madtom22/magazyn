39adf56b3ed975146616a09f3a7b8c279d62af88
Tomasz Madej <tomasz@madej.ovh>
2022-10-04 20:35:25 +0200

    Add def get_items

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tkinter import E
+from statistics import quantiles
+
 
 items = [
 {'name':'śrubka','quantity':100,'unit':'szt','unit_price':4},
@@ -6,8 +7,16 @@ items = [
 {'name':'nakrętka','quantity':250,'unit':'szt','unit_price':2},
 {'name':'kształtka','quantity':50,'unit':'szt','unit_price':3.5}
 ]
-    
 
+def get_items():
+    print('Name\t\tQuantity\t\tUnit\t\tUnit Price (PLN)')
+    print('____\t\t________\t\t____\t\t__________')
+    #for name, quantity, unit, unit_price in list(items()):
+     #   print(name, quantity, unit, unit_price)
+
+    for i in range(len(items)):
+        y = items[i]
+        print(y['name'],'\t\t',y['quantity'],'\t\t',y['unit'],'\t\t',y['unit_price'])
 
 if __name__ == "__main__":
 
@@ -16,7 +25,8 @@ if __name__ == "__main__":
     while x != 'exit':
         
         if x == 'show':
-            print(items)
+            print(get_items())
+            
         x = str(input('Podaj co chcesz zrobić? '))
     
     print('Koniec...Narka!')
